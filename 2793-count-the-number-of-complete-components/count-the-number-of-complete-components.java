52a12faed03937cf6aa2ae15c7a29288eea5adcc
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-07-11 23:11:40 +0530

    Time: 8 ms (94.15%) | Memory: 47.5 MB (88.79%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,56 +1,40 @@
-import java.util.*;
-
 class Solution {
     public int countCompleteComponents(int n, int[][] edges) {
-        ArrayList<Integer>[] graph = new ArrayList[n];
+        List<Integer>[] adjList = new ArrayList[n];
 
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+        for (int vertex = 0; vertex < n; vertex++) {
+            adjList[vertex] = new ArrayList<>();
         }
 
-        for (int[] e : edges) {
-            int a = e[0];
-            int b = e[1];
-
-            graph[a].add(b);
-            graph[b].add(a);
+        for (int i = 0; i < edges.length; i++) {
+            adjList[edges[i][0]].add(edges[i][1]);
+            adjList[edges[i][1]].add(edges[i][0]);
         }
 
+        int counter = 0;
         boolean[] visited = new boolean[n];
-        int ans = 0;
-
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                ArrayList<Integer> component = new ArrayList<>();
-                dfs(i, graph, visited, component);
+            if (visited[i] == true)
+                continue;
+            int[] componentInfo = new int[2];
+            dfs(adjList, visited, i, componentInfo);
+            if (componentInfo[0] * (componentInfo[0] - 1) == componentInfo[1])
+                counter++;
 
-                int nodes = component.size();
-                int degreeSum = 0;
-
-                for (int node : component) {
-                    degreeSum += graph[node].size();
-                }
-
-                int edgesCount = degreeSum / 2;
-
-                if (edgesCount == nodes * (nodes - 1) / 2) {
-                    ans++;
-                }
-            }
         }
+        return counter;
 
-        return ans;
     }
 
-    void dfs(int node, ArrayList<Integer>[] graph, boolean[] visited,
-            ArrayList<Integer> component) {
+    public void dfs(List<Integer>[] adjList, boolean[] visited, int ind, int[] componentInfo) {
+        visited[ind] = true;
+        componentInfo[0]++;
+        componentInfo[1] += adjList[ind].size();
 
-        visited[node] = true;
-        component.add(node);
-
-        for (int next : graph[node]) {
-            if (!visited[next]) {
-                dfs(next, graph, visited, component);
+        for (int i = 0; i < adjList[ind].size(); i++) {
+            int node = adjList[ind].get(i);
+            if (visited[node] == false) {
+                dfs(adjList, visited, node, componentInfo);
             }
         }
     }
