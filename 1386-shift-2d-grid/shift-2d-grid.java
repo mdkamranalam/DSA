767b60f0c43b7b59f2150ad8e1657289e32662cb
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-07-20 16:18:03 +0530

    Time: 5 ms (83.45%) | Memory: 47.1 MB (79.54%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,20 +1,28 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int m = grid.length, n = grid[0].length;
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < m; ++i) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < n; ++j) {
-                row.add(0);
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+        k = k % total;
+        int[][] res = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+                int newidx = (idx + k) % total;
+
+                int row = newidx / n;
+                int col = newidx % n;
+                res[row][col] = grid[i][j];
+
             }
-            ans.add(row);
         }
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int idx = (i * n + j + k) % (m * n);
-                int x = idx / n, y = idx % n;
-                ans.get(x).set(y, grid[i][j]);
+        for (int i = 0; i < m; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                list.add(res[i][j]);
             }
+            ans.add(list);
         }
         return ans;
     }
