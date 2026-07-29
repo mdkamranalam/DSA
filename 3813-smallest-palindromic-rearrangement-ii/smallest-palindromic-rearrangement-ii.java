0632e7486e4ad4b1356db3ad73db68d6d27accbb
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-07-29 15:24:33 +0530

    Time: 15 ms (93.55%) | Memory: 47 MB (90.32%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.java
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.java
@@ -1,83 +1,84 @@
 class Solution {
-    public String smallestPalindrome(String inputStr, int K) {
-        int[] frequency = new int[26];
-        for (int i = 0; i < inputStr.length(); i++) {
-            char ch = inputStr.charAt(i);
-            frequency[ch - 'a']++;
-        }
-        char mid = 0;
+    static final long LIMIT = 1_000_000L;
+
+    public String smallestPalindrome(String s, int k) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+
+        int[] half = new int[26];
+        String mid = "";
+
+        int halfLen = 0;
         for (int i = 0; i < 26; i++) {
-            if (frequency[i] % 2 == 1) {
-                mid = (char) ('a' + i);
-                frequency[i]--;
-                break;
-            }
+            half[i] = freq[i] / 2;
+            halfLen += half[i];
+            if ((freq[i] & 1) == 1)
+                mid = String.valueOf((char) ('a' + i));
         }
-        int[] halfFreq = new int[26];
-        int halfLength = 0;
-        for (int i = 0; i < 26; i++) {
-            halfFreq[i] = frequency[i] / 2;
-            halfLength += halfFreq[i];
-        }
-        long totalPerms = multinomial(halfFreq);
-        if (K > totalPerms)
+
+        if (countWays(half, halfLen) < k)
             return "";
-        StringBuilder firstHalfBuilder = new StringBuilder();
-        for (int i = 0; i < halfLength; i++) {
+
+        StringBuilder left = new StringBuilder();
+
+        while (halfLen > 0) {
             for (int c = 0; c < 26; c++) {
-                if (halfFreq[c] > 0) {
-                    halfFreq[c]--;
-                    long perms = multinomial(halfFreq);
-                    if (perms >= K) {
-                        firstHalfBuilder.append((char) ('a' + c));
-                        break;
-                    } else {
-                        K -= perms;
-                        halfFreq[c]++;
-                    }
+                if (half[c] == 0)
+                    continue;
+
+                half[c]--;
+                long ways = countWays(half, halfLen - 1);
+
+                if (ways >= k) {
+                    left.append((char) ('a' + c));
+                    halfLen--;
+                    break;
+                } else {
+                    k -= ways;
+                    half[c]++;
                 }
             }
         }
-        String firstHalf = firstHalfBuilder.toString();
-        String revHalf = new StringBuilder(firstHalf).reverse().toString();
-        String result;
-        if (mid == 0) {
-            result = firstHalf + revHalf;
-        } else {
-            result = firstHalf + mid + revHalf;
-        }
-        return result;
+
+        StringBuilder ans = new StringBuilder();
+        ans.append(left);
+        ans.append(mid);
+        ans.append(left.reverse());
+
+        return ans.toString();
     }
 
-    static long maxK = 1000001;
-
-    public long multinomial(int[] counts) {
-        int tot = 0;
-        for (int cnt : counts) {
-            tot += cnt;
-        }
+    private long countWays(int[] half, int total) {
         long res = 1;
-        for (int i = 0; i < 26; i++) {
-            int cnt = counts[i];
-            res = res * binom(tot, cnt);
-            if (res >= maxK)
-                return maxK;
-            tot -= cnt;
+
+        int remain = total;
+
+        for (int x : half) {
+            if (x == 0)
+                continue;
+            res *= comb(remain, x);
+            if (res > LIMIT)
+                return LIMIT;
+            remain -= x;
         }
-        return res;
+
+        return Math.min(res, LIMIT);
     }
 
-    public long binom(int n, int k) {
-        if (k > n)
+    private long comb(int n, int r) {
+        if (r > n)
             return 0;
-        if (k > n - k)
-            k = n - k;
-        long result = 1;
-        for (int i = 1; i <= k; i++) {
-            result = result * (n - i + 1) / i;
-            if (result >= maxK)
-                return maxK;
+        r = Math.min(r, n - r);
+
+        long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - r + i) / i;
+            if (ans > LIMIT)
+                return LIMIT;
         }
-        return result;
+
+        return Math.min(ans, LIMIT);
     }
 }
