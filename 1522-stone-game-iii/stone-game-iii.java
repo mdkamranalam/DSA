70632f582a40c0588d6e705d7a05f4131ff158f0
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-03 16:31:08 +0530

    Time: 2 ms (100.00%) | Memory: 81 MB (98.58%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,21 +1,21 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
-        int n = stoneValue.length;
-        int[] dp = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = Integer.MIN_VALUE;
-            int sum = 0;
-            for (int k = 0; k < 3 && i + k < n; k++) {
-                sum += stoneValue[i + k];
-                dp[i] = Math.max(dp[i], sum - dp[i + k + 1]);
-            }
+        int sufSum = 0;
+        int f1 = 0;
+        int f2 = 0;
+        int f3 = 0;
+        for (int i = stoneValue.length - 1; i >= 0; i--) {
+            sufSum += stoneValue[i];
+            int newF = sufSum - Math.min(Math.min(f1, f2), f3);
+            f3 = f2;
+            f2 = f1;
+            f1 = newF;
         }
-        if (dp[0] > 0) {
-            return "Alice";
+
+        int diff = f1 - (sufSum - f1);
+        if (diff == 0) {
+            return "Tie";
         }
-        if (dp[0] < 0) {
-            return "Bob";
-        }
-        return "Tie";
+        return diff > 0 ? "Alice" : "Bob";
     }
 }
