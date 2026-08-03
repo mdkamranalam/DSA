c3fa1cbae43be4da3cc1e64f279fb4d020ffbc57
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-03 16:30:18 +0530

    Time: 10 ms (83.24%) | Memory: 85.3 MB (76.70%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,38 +1,21 @@
 class Solution {
-    private int[] stoneValue;
-    private Integer[] f;
-    private int n;
-
     public String stoneGameIII(int[] stoneValue) {
-        this.stoneValue = stoneValue;
-        this.n = stoneValue.length;
-        this.f = new Integer[n];
-
-        int res = dfs(0);
-
-        if (res == 0) {
-            return "Tie";
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                sum += stoneValue[i + k];
+                dp[i] = Math.max(dp[i], sum - dp[i + k + 1]);
+            }
         }
-        return res > 0 ? "Alice" : "Bob";
-    }
-
-    private int dfs(int i) {
-        if (i >= n) {
-            return 0;
+        if (dp[0] > 0) {
+            return "Alice";
         }
-
-        if (f[i] != null) {
-            return f[i];
+        if (dp[0] < 0) {
+            return "Bob";
         }
-
-        int ans = Integer.MIN_VALUE;
-        int s = 0;
-
-        for (int j = i; j < i + 3 && j < n; j++) {
-            s += stoneValue[j];
-            ans = Math.max(ans, s - dfs(j + 1));
-        }
-
-        return f[i] = ans;
+        return "Tie";
     }
 }
