7766df45dbeb73f2c9fbf7a767fb9494be5c77f9
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-05 15:11:54 +0530

    Time: 61 ms (81.73%) | Memory: 269.1 MB (96.15%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -1,43 +1,57 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
-        List<Integer>[] g = new ArrayList[n];
-        Arrays.setAll(g, i -> new ArrayList<>());
-
-        for (int[] e : invocations) {
-            g[e[0]].add(e[1]);
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
         }
 
-        boolean[] isSuspicous = new boolean[n];
-
-        dfs(k, g, isSuspicous);
-
         for (int[] e : invocations) {
-            if (!isSuspicous[e[0]] && isSuspicous[e[1]]) {
-                List<Integer> ans = new ArrayList<>(n);
-                for (int i = 0; i < n; i++) {
-                    ans.add(i);
-                }
+            adj[e[0]].add(e[1]);
+        }
 
-                return ans;
+        boolean[] suspicious = new boolean[n];
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(k);
+        suspicious[k] = true;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+
+            for (int v : adj[u]) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                for (int v : adj[i]) {
+                    if (suspicious[v]) {
+                        List<Integer> ans = new ArrayList<>();
+                        for (int j = 0; j < n; j++) {
+                            ans.add(j);
+                        }
+                        return ans;
+                    }
+                }
             }
         }
 
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (!isSuspicous[i]) {
+            if (!suspicious[i]) {
                 ans.add(i);
             }
         }
+
         return ans;
-    }
-
-    private void dfs(int i, List<Integer>[] g, boolean[] isSuspicous) {
-        isSuspicous[i] = true;
-
-        for (int y : g[i]) {
-            if (!isSuspicous[y]) {
-                dfs(y, g, isSuspicous);
-            }
-        }
     }
 }
