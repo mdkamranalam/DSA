070af9cb38b116c0499d1c68a7dd37d5410fee5d
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-08 22:40:52 +0530

    Time: 31 ms (97.22%) | Memory: 137.5 MB (52.78%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
-        int[] ans = new int[word2.length()];
-        // last[j] := the index i of the last occurrence in word1, where
-        // word1[i] == word2[j]
-        int[] last = new int[word2.length()];
-        Arrays.fill(last, -1);
-
-        int i = word1.length() - 1;
-        int j = word2.length() - 1;
-        while (i >= 0 && j >= 0) {
-            if (word1.charAt(i) == word2.charAt(j))
-                last[j--] = i;
-            --i;
+        int n = word1.length(), m = word2.length();
+        int[] suf = new int[m + 1];
+        suf[m] = n;
+        int p = n - 1;
+        for (int j = m - 1; j >= 0; j--) {
+            while (p >= 0 && word1.charAt(p) != word2.charAt(j)) {
+                p--;
+            }
+            suf[j] = p--;
         }
-
-        boolean canSkip = true;
-        j = 0;
-        for (i = 0; i < word1.length(); ++i) {
-            if (j == word2.length())
-                break;
+        int[] res = new int[m];
+        int j = 0;
+        boolean used = false;
+        for (int i = 0; i < n && j < m; i++) {
             if (word1.charAt(i) == word2.charAt(j)) {
-                ans[j++] = i;
-            } else if (canSkip && (j == word2.length() - 1 || i < last[j + 1])) {
-                canSkip = false;
-                ans[j++] = i;
+                res[j++] = i;
+            } else if (!used && i + 1 <= suf[j + 1]) {
+                used = true;
+                res[j++] = i;
             }
         }
-
-        return j == word2.length() ? ans : new int[0];
+        return j == m ? res : new int[0];
     }
 }
