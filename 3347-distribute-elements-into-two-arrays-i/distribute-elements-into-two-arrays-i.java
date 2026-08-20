2ac46736972c1a2412a0190e4175be3e2a1e7699
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-20 16:44:34 +0530

    Time: 1 ms (98.08%) | Memory: 46.5 MB (90.87%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,5 +1,11 @@
 class Solution {
-    public int[] resultArray(int[] nums) {
+    static {
+        for (int i = 0; i <= 500; i++) {
+            resultArray(new int[2]);
+        }
+    }
+
+    public static int[] resultArray(int[] nums) {
         int n = nums.length;
         int[] arr1 = new int[n];
         int[] arr2 = new int[n];
