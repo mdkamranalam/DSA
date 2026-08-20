2c58fa00328c7beef1bd7d1a1c279246587e6dd1
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-20 16:45:07 +0530

    Time: 1 ms (98.08%) | Memory: 46.7 MB (42.79%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -8,23 +8,22 @@ class Solution {
     public static int[] resultArray(int[] nums) {
         int n = nums.length;
         int[] arr1 = new int[n];
-        int[] arr2 = new int[n];
+        int[] arr2 = new int[n - 1];
         arr1[0] = nums[0];
         arr2[0] = nums[1];
-        int i = 0, j = 0;
 
-        for (int k = 2; k < n; ++k) {
-            if (arr1[i] > arr2[j]) {
-                arr1[++i] = nums[k];
+        int arr1Pointer = 0;
+        int arr2Pointer = 0;
+        for (int i = 2; i < n; i++) {
+            if (arr1[arr1Pointer] > arr2[arr2Pointer]) {
+                arr1[++arr1Pointer] = nums[i];
             } else {
-                arr2[++j] = nums[k];
+                arr2[++arr2Pointer] = nums[i];
             }
         }
-
-        for (int k = 0; k <= j; ++k) {
-            arr1[++i] = arr2[k];
+        for (int i = 0; i <= arr2Pointer; i++) {
+            arr1[++arr1Pointer] = arr2[i];
         }
-
         return arr1;
     }
 }
