c0797de4ed6cbc5bc89f33466bb121c587fcc3b5
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-23 19:51:26 +0530

    Time: 5 ms (100.00%) | Memory: 46.8 MB (83.94%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -4,17 +4,14 @@ class Solution {
         int[] left = get(num.substring(0, n / 2));
         int[] right = get(num.substring(n / 2, n));
 
-        int n0 = left[0],
-                q0 = left[1];
-        int n1 = right[0],
-                q1 = right[1];
+        int n0 = left[0], q0 = left[1];
+        int n1 = right[0], q1 = right[1];
 
         return (q0 + q1) % 2 == 1 || n0 - n1 != ((q1 - q0) * 9) / 2;
     }
 
     private int[] get(String s) {
-        int nn = 0,
-                qq = 0;
+        int nn = 0, qq = 0;
         for (char ch : s.toCharArray()) {
             if (ch == '?') {
                 qq++;
