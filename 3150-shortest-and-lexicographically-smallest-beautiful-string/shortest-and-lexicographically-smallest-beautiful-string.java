e8d48a7900c5d646465b93d1ece227cfc0abd632
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-08-26 18:42:57 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (75.84%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,22 +1,37 @@
-class Solution {
+public class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        int i = 0, j = 0, cnt = 0;
-        int n = s.length();
-        String ans = "";
-        while (j < n) {
-            cnt += s.charAt(j) - '0';
-            while (cnt > k || (i < j && s.charAt(i) == '0')) {
-                cnt -= s.charAt(i) - '0';
-                ++i;
+        char[] input = s.toCharArray();
+
+        int head = -1;
+        int tail = -1;
+
+        int back = 0;
+        int countOnes = 0;
+
+        for (int front = 0; front < input.length; ++front) {
+            countOnes += input[front] - '0';
+            if (countOnes < k) {
+                continue;
             }
-            ++j;
-            String t = s.substring(i, j);
-            if (cnt == k
-                    && ("".equals(ans) || j - i < ans.length()
-                            || (j - i == ans.length() && t.compareTo(ans) < 0))) {
-                ans = t;
+
+            while (back < front && input[back] == '0') {
+                countOnes -= input[back] - '0';
+                ++back;
+            }
+
+            if (head == -1 || head - tail + 1 > front - back + 1) {
+                head = front;
+                tail = back;
+            } else if (head - tail + 1 == front - back + 1
+                    && s.substring(tail, head + 1).compareTo(s.substring(back, front + 1)) > 0) {
+                head = front;
+                tail = back;
+            }
+            while (back < front && countOnes == k) {
+                countOnes -= input[back] - '0';
+                ++back;
             }
         }
-        return ans;
+        return head != -1 ? s.substring(tail, head + 1) : "";
     }
 }
