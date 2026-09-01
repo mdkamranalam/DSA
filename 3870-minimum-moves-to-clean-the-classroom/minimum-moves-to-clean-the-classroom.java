648bd6497f16a31479138a9455f9c4e081dbfbc4
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-09-01 12:27:21 +0530

    Time: 155 ms (73.81%) | Memory: 61.2 MB (76.19%) - LeetSync

--- 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.java
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.java
@@ -1,58 +1,141 @@
 class Solution {
-    public int minMoves(String[] classroom, int energy) {
-        int m = classroom.length, n = classroom[0].length();
-        int[][] d = new int[m][n];
-        int x = 0, y = 0, cnt = 0;
-        for (int i = 0; i < m; i++) {
-            String row = classroom[i];
-            for (int j = 0; j < n; j++) {
-                char c = row.charAt(j);
-                if (c == 'S') {
+
+    class node {
+        int i;
+        int j;
+        int step;
+        int liter;
+        int energy;
+
+        public node(int i, int j, int step, int liter, int energy) {
+            this.i = i;
+            this.j = j;
+            this.step = step;
+            this.liter = liter;
+            this.energy = energy;
+        }
+    }
+
+    public int minMoves(String[] classroom, int e) {
+
+        int n = classroom.length;
+        int m = classroom[0].length();
+
+        int x = -1;
+        int y = -1;
+
+        int l = 0;
+
+        int[][] id = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                id[i][j] = -1;
+
+                char ch = classroom[i].charAt(j);
+
+                if (ch == 'S') {
                     x = i;
                     y = j;
-                } else if (c == 'L') {
-                    d[i][j] = cnt;
-                    cnt++;
+                }
+
+                if (ch == 'L') {
+                    id[i][j] = l;
+                    l++;
                 }
             }
         }
-        if (cnt == 0) {
-            return 0;
-        }
-        boolean[][][][] vis = new boolean[m][n][energy + 1][1 << cnt];
-        List<int[]> q = new ArrayList<>();
-        q.add(new int[] { x, y, energy, (1 << cnt) - 1 });
-        vis[x][y][energy][(1 << cnt) - 1] = true;
-        int[] dirs = { -1, 0, 1, 0, -1 };
-        int ans = 0;
-        while (!q.isEmpty()) {
-            List<int[]> t = q;
-            q = new ArrayList<>();
-            for (int[] state : t) {
-                int i = state[0], j = state[1], curEnergy = state[2], mask = state[3];
-                if (mask == 0) {
-                    return ans;
+
+        int total = 1 << l;
+
+        int[][][] visited = new int[n][m][total];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < total; k++) {
+                    visited[i][j][k] = -1;
                 }
-                if (curEnergy <= 0) {
+            }
+        }
+
+        Queue<node> pq = new LinkedList<>();
+
+        pq.add(new node(x, y, 0, 0, e));
+
+        visited[x][y][0] = e;
+
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+
+        while (pq.size() > 0) {
+
+            node abc = pq.poll();
+
+            int i = abc.i;
+            int j = abc.j;
+            int step = abc.step;
+            int liter = abc.liter;
+            int energy = abc.energy;
+
+            if (classroom[i].charAt(j) == 'L') {
+                int idd = id[i][j];
+
+                liter = liter | (1 << idd);
+            }
+
+            if (liter == total - 1) {
+                return step;
+            }
+
+            if (classroom[i].charAt(j) == 'R') {
+                energy = e;
+            }
+
+            if (energy == 0) {
+                continue;
+            }
+
+            for (int d = 0; d < 4; d++) {
+
+                int ni = i + dx[d];
+                int nj = j + dy[d];
+
+                if (ni < 0 || ni >= n || nj < 0 || nj >= m) {
                     continue;
                 }
-                for (int k = 0; k < 4; k++) {
-                    int nx = i + dirs[k], ny = j + dirs[k + 1];
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && classroom[nx].charAt(ny) != 'X') {
-                        int nxtEnergy = classroom[nx].charAt(ny) == 'R' ? energy : curEnergy - 1;
-                        int nxtMask = mask;
-                        if (classroom[nx].charAt(ny) == 'L') {
-                            nxtMask &= ~(1 << d[nx][ny]);
-                        }
-                        if (!vis[nx][ny][nxtEnergy][nxtMask]) {
-                            vis[nx][ny][nxtEnergy][nxtMask] = true;
-                            q.add(new int[] { nx, ny, nxtEnergy, nxtMask });
-                        }
-                    }
+
+                if (classroom[ni].charAt(nj) == 'X') {
+                    continue;
                 }
+
+                int newEnergy = energy - 1;
+                int newLiter = liter;
+
+                if (classroom[ni].charAt(nj) == 'L') {
+                    int idd = id[ni][nj];
+
+                    newLiter = newLiter | (1 << idd);
+                }
+
+                if (classroom[ni].charAt(nj) == 'R') {
+                    newEnergy = e;
+                }
+
+                if (visited[ni][nj][newLiter] >= newEnergy) {
+                    continue;
+                }
+
+                visited[ni][nj][newLiter] = newEnergy;
+
+                pq.add(new node(
+                        ni,
+                        nj,
+                        step + 1,
+                        newLiter,
+                        newEnergy));
             }
-            ans++;
         }
+
         return -1;
     }
 }
