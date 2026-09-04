7ec363f87937d60daafe6cc571e1246bdf211cf5
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-09-04 18:59:25 +0530

    Time: 1 ms (99.79%) | Memory: 45.9 MB (97.45%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -3,11 +3,9 @@ class Solution {
         int n = nums.length;
         int[] right = new int[n];
         right[n - 1] = nums[n - 1];
-
         for (int i = n - 2; i >= 0; i--) {
             right[i] = Math.min(right[i + 1], nums[i]);
         }
-
         int left = 0;
         for (int i = 0; i < n; i++) {
             left = Math.max(left, nums[i]);
@@ -17,4 +15,5 @@ class Solution {
         }
         return -1;
     }
+
 }
