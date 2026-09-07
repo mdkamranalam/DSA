f948d946b0de9cd4adb9dade6981f5e02f21ed5c
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-09-07 18:50:08 +0530

    Time: 4 ms (70.57%) | Memory: 43.2 MB (81.56%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -2,16 +2,12 @@ class Solution {
     public int distinctSubseqII(String s) {
         final int mod = (int) 1e9 + 7;
         int[] f = new int[26];
-        for (int i = 0; i < s.length(); ++i) {
-            int x = 1;
-            for (int v : f) {
-                x = (x + v) % mod;
-            }
-            f[s.charAt(i) - 'a'] = x;
-        }
         int ans = 0;
-        for (int v : f) {
-            ans = (ans + v) % mod;
+        for (int i = 0; i < s.length(); ++i) {
+            int j = s.charAt(i) - 'a';
+            int add = (ans + 1 + mod - f[j]) % mod;
+            ans = (ans + add) % mod;
+            f[j] = (f[j] + add) % mod;
         }
         return ans;
     }
