c2b342562ba7699881ad037fa17f1b0cb01ac371
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-09-13 23:16:47 +0530

    Time: 58 ms (53.03%) | Memory: 44.4 MB (85.23%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,22 +1,33 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n = img1.length;
-        Map<List<Integer>, Integer> cnt = new HashMap<>();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (img1[i][j] == 1) {
-                    for (int h = 0; h < n; ++h) {
-                        for (int k = 0; k < n; ++k) {
-                            if (img2[h][k] == 1) {
-                                List<Integer> t = List.of(i - h, j - k);
-                                ans = Math.max(ans, cnt.merge(t, 1, Integer::sum));
-                            }
+        int max = 0;
+
+        for (int r = -n + 1; r < n; r++) {
+            for (int c = -n + 1; c < n; c++) {
+
+                int overlap = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        int x = i + r;
+                        int y = j + c;
+
+                        if (x >= 0 && x < n &&
+                                y >= 0 && y < n &&
+                                img1[i][j] == 1 &&
+                                img2[x][y] == 1) {
+
+                            overlap++;
                         }
                     }
                 }
+
+                max = Math.max(max, overlap);
             }
         }
-        return ans;
+
+        return max;
     }
 }
