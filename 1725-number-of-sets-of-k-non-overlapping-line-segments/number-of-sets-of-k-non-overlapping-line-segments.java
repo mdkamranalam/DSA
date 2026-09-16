d3f833b04762760e2cd68239b20b38061ce83ffe
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-09-16 21:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (89.03%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,19 +1,24 @@
 class Solution {
-    public int numberOfSets(int n, int k) {
-        final int mod = (int) 1e9 + 7;
-        int[][] f = new int[n + 1][k + 1];
-        int[][] g = new int[n + 1][k + 1];
-        f[1][0] = 1;
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 0; j <= k; ++j) {
-                f[i][j] = (f[i - 1][j] + g[i - 1][j]) % mod;
-                g[i][j] = g[i - 1][j];
-                if (j > 0) {
-                    g[i][j] = (g[i][j] + f[i - 1][j - 1]) % mod;
-                    g[i][j] = (g[i][j] + g[i - 1][j - 1]) % mod;
-                }
-            }
+    private static final long MOD = 1000000007L;
+
+    private long quickPow(long a, long e) {
+        long result = 1;
+        while (e > 0) {
+            if ((e & 1) != 0)
+                result = (result * a) % MOD;
+            a = (a * a) % MOD;
+            e >>= 1;
         }
-        return (f[n][k] + g[n][k]) % mod;
+        return result;
+    }
+
+    public int numberOfSets(int n, int k) {
+        int m = 2 * k;
+        long numerator = 1, denominator = 1;
+        for (int i = 1; i <= m; i++) {
+            numerator = (numerator * (n + k - i)) % MOD;
+            denominator = (denominator * i) % MOD;
+        }
+        return (int) ((numerator * quickPow(denominator, MOD - 2)) % MOD);
     }
 }
