9a72e91d756a918a24ca40c81b8e78904d8a6787
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2026-09-23 21:24:28 +0530

    Time: 4 ms (98.07%) | Memory: 102.3 MB (29.35%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -4,15 +4,15 @@ class Solution {
         for (int v : nums) {
             s += v;
         }
-        Map<Integer, Integer> vis = new HashMap<>();
-        vis.put(0, -1);
         int mx = -1, t = 0;
         int n = nums.length;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0, j = 0; i < n; ++i) {
             t += nums[i];
-            vis.putIfAbsent(t, i);
-            if (vis.containsKey(t - s)) {
-                mx = Math.max(mx, i - vis.get(t - s));
+            while (j <= i && t > s) {
+                t -= nums[j++];
+            }
+            if (t == s) {
+                mx = Math.max(mx, i - j + 1);
             }
         }
         return mx == -1 ? -1 : n - mx;
